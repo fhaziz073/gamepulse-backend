37ec71c80e867b2a40d014e3738bf79ba3ed5320
fhaziz073 <faziz2@stevens.edu>
2026-02-10 16:57:15 -0500

    Changed findAll for Calendar provider to get all games for the next month instead of using hardcoded lines

--- src/calendar/calendar.service.ts
+++ src/calendar/calendar.service.ts
@@ -7,6 +7,7 @@ export type Event = {
   title: string;
   summary?: string;
 };
+
 @Injectable()
 export class CalendarService {
   private apiKey = process.env.SPORTS_API_KEY as string;
@@ -14,16 +15,27 @@ export class CalendarService {
   private readonly events: Event[] = [];
 
   async findAll(): Promise<Event[]> {
+    let date1 = new Date();
+    const offset = date1.getTimezoneOffset();
+    date1 = new Date(date1.getTime() - offset * 60 * 1000);
+    let date2 = new Date();
+    date2.setMonth(date2.getMonth() + 1);
+    const offset2 = date2.getTimezoneOffset();
+    date2 = new Date(date2.getTime() - offset2 * 60 * 1000);
     const games = await this.api.nba.getGames({
       team_ids: [1],
-      start_date: '2026-02-01',
+      start_date: date1.toISOString().split('T')[0],
+      end_date: date2.toISOString().split('T')[0],
     });
     const gamesData = games.data;
     for (const game of gamesData) {
+      const gameLengthInMs = 2.5 * 60 * 60 * 1000;
+      const gameStart = new Date(game.status);
+      const gameEnd = new Date(gameStart.getTime() + gameLengthInMs);
       this.events.push({
-        start: game.date,
-        end: game.date,
-        title: `${game.home_team.name} + vs + ${game.visitor_team.name}`,
+        start: gameStart.toString(),
+        end: gameEnd.toString(),
+        title: `${game.visitor_team.name} at ${game.home_team.name}`,
       });
     }
     return this.events;
