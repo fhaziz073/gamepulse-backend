7e55ac56ec780db64e224a778083f44433cd8bb5
fhaziz073 <faziz2@stevens.edu>
2026-04-06 17:07:13 -0400

    Deleting old login controller that doesn't do anything

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CalendarController } from './calendar/calendar.controller';
 import { CalendarService } from './calendar/calendar.service';
-import { LoginController } from './login/login.controller';
 import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 import { DatabaseModule } from './database/database.module';
@@ -14,12 +13,7 @@ import { TasksService } from './tasks/tasks.service';
 
 @Module({
   imports: [DatabaseModule, AuthModule, ScheduleModule.forRoot()],
-  controllers: [
-    AppController,
-    CalendarController,
-    LoginController,
-    UserController,
-  ],
+  controllers: [AppController, CalendarController, UserController],
   providers: [
     AppService,
     CalendarService,
